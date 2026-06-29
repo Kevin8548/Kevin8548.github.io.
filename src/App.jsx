b52e50ae4793246b4127b70f90f31fa315bb9a54
Kevin8548 <kevinarroyolopez231@gmail.com>
2026-06-28 22:25:55 -0600

    Pages1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Navbar } from "@/layout/navbar";
+import { Navbar } from "@/layout/Navbar";
 import { Hero } from "@/sections/Hero.jsx";
 import { About } from "@/sections/About";
 import { Projects } from "@/sections/Projects";
